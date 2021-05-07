0f15818d6361558e163b4e82bf239045194d0937
Ryan Finken <ryanfinken338@gmail.com>
2021-05-06 20:03:58 -0500

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,8 @@
 package test;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import main.BalancedBrackets;
 
 public class BalancedBracketsTest {
 
@@ -12,5 +12,59 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void noBracketsPlusTextReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("test"));
+    }
+
+    @Test
+    public void bracketsOutOfOrderReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void bracketsInOrderPlusTextReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("t[es]t"));
+    }
+
+    @Test
+    public void nestedBracketsInOrderReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void nestedBracketsPlusTextReturnsTrue() {
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[t[es]t]"));
+    }
+
+    @Test
+    public void openingBracketReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void openingBracketPlusTextReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("t[est"));
+    }
+
+    @Test
+    public void closingBracketReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void closingBracketPlusTextReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("te]st"));
+    }
 
 }
